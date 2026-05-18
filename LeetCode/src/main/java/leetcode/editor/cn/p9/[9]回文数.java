e2e4c2548b4fc38ab8b4f64ae1968ed794399f56
chen <zhiduoming@qq.com>
2026-05-19 00:01:53 +0800

    新增 LeetCode 回文数题解

---
+++ LeetCode/src/main/java/leetcode/editor/cn/p9/[9]回文数.java
@@ -0,0 +1,79 @@
+package leetcode.editor.cn.p9;
+
+import leetcode.editor.cn.common.ListNode;
+import leetcode.editor.cn.common.TreeNode;
+
+import java.util.*;
+
+//给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。 
+//
+// 回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。 
+//
+// 
+// 例如，121 是回文，而 123 不是。 
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：x = 121
+//输出：true
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：x = -121
+//输出：false
+//解释：从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：x = 10
+//输出：false
+//解释：从右向左读, 为 01 。因此它不是一个回文数。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// -2³¹ <= x <= 2³¹ - 1 
+// 
+//
+// 
+//
+// 进阶：你能不将整数转为字符串来解决这个问题吗？ 
+//
+// Related Topics 数学 👍 3185 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+
+        Deque<Integer> stack = new ArrayDeque<>();
+        int n = x;
+        while (n > 0) {
+            int low = n % 10;
+            stack.add(low);
+            n /= 10;
+        }
+        while (x > 0) {
+            Integer low = x % 10;
+            Integer high = stack.poll();
+            if(!low.equals(high)){
+                return false;
+            }
+            x/=10;
+        }
+        return true;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
